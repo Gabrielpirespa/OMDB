d3657c899b63a3c101f559522ea57f29886b6603
Gabriel Pires Parreira <gabrielpirespa@hotmail.com>
2023-02-10 09:16:34 -0300

    coments update

--- app/src/test/java/com/iniciandoaviddadev/omdb/MovieDescriptionTests.kt
+++ app/src/test/java/com/iniciandoaviddadev/omdb/MovieDescriptionTests.kt
@@ -1,18 +1,21 @@
 package com.iniciandoaviddadev.omdb
 
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
 class MovieDescriptionTests {
 
-//    @Test
-//    fun `should call the Api when receive a imdbId`()= runTest {
-//        //Arrange
-//        mockkConstructor(SecondFragment::class)
-//
-//        val plotService = mockk<PlotService>()
-//        val movieDescriptionRemoteDataSource = MovieDescriptionRemoteDataSource(plotService)
-//        val args: SecondFragment by NavArgs
-//
-//        //Action
-//
-//        //Assert
-//    }
+    @Test
+    fun `should call the Api when receive a imdbId`()= runTest {
+        //Arrange
+        /*mockkConstructor(SecondFragment::class)
+
+        val plotService = mockk<PlotService>()
+        val movieDescriptionRemoteDataSource = MovieDescriptionRemoteDataSource(plotService)
+        val args: SecondFragment by NavArgs*/ //Não sei testar o Safe Args ainda, estudando alguma forma
+
+        //Action
+
+        //Assert
+    }
 }
